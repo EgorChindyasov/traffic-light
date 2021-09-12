1e4712ffc797658fec102c639ac35ddb1ab744c8
Egor O. Chindyasov <egor@chindyasov.ru>
2021-09-12 20:17:32 +0300

    created component of counter

---
+++ src/components/Сountdown.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="count">
+        <span class="count__text">{{ time }}</span>
+    </div>    
+</template>
+
+<script>
+export default {
+    props: ['time']
+}
+</script>
+
+<style scoped>
+.count {
+    margin: 0 auto;
+}
+
+.count__text {
+    color: black;
+    font-weight: bold;
+}
+</style>
